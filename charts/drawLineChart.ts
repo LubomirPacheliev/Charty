369de7ef6d47c9dc6f1b9df210f10a30ae61fe17
LubomirP <lubomirpacheliev@gmail.com>
2022-06-12 12:42:23 +0300

    some ts refactoring

--- charts/drawLineChart.ts
+++ charts/drawLineChart.ts
@@ -1,5 +1,4 @@
-//@ts-ignore
-const d3 = window.d3 || import('d3');
+import d3 from 'd3';
 import getCandleData from './getCandleData';
 import responsivefy from './responsivefy';
 
@@ -15,16 +14,13 @@ type Options = {
 
 type Datum = {
     date?: Date,
-    open?: Number | String,
-    high?: Number | String,
-    low?: Number | String,
-    close?: Number | String
+    open?: Number | string,
+    high?: Number | string,
+    low?: Number | string,
+    close?: Number | string
 };
 
-console.log('before def of func')
-
 const drawLineChart = async (symbol: string, interval: string, options: Options) => {
-    console.log(getCandleData);
     const data = await getCandleData(symbol, interval);
 
     data.map((datum: Datum) => {
@@ -50,8 +46,8 @@ const drawLineChart = async (symbol: string, interval: string, options: Options)
         left = Number(options.marginLeft ? options.marginLeft : 50),
     }
 
-    const width = Number(options.chartWidth ? options.chartWidth : ( window.innerWidth - margin.left - margin.right));
-    const height = Number(options.chartHeight ? options.chartHeight : ( window.innerHeight - margin.top - margin.bottom));
+    const width = Number(options.chartWidth ? options.chartWidth : (window.innerWidth - margin.left - margin.right));
+    const height = Number(options.chartHeight ? options.chartHeight : (window.innerHeight - margin.top - margin.bottom));
 
     const svg = d3
         .select('.chart')
@@ -62,13 +58,13 @@ const drawLineChart = async (symbol: string, interval: string, options: Options)
         .append('g')
         .attr('transform', `translate(${margin.left}), ${margin.top}`);
 
-    const xMin = d3.min(data, (datum: Datum) => datum.date);
-    const xMax = d3.max(data, (datum: Datum) => datum.date);
-    const yMin = d3.min(data, (datum: Datum) => datum.close);
-    const yMax = d3.max(data, (datum: Datum) => datum.close);
+    const xMin = Number(d3.min(data, (datum: Datum) => datum.date));
+    const xMax = Number(d3.max(data, (datum: Datum) => datum.date));
+    const yMin = Number(d3.min(data, (datum: Datum) => (datum.close)?.toString()));
+    const yMax = Number(d3.max(data, (datum: Datum) => (datum.close)?.toString()));
 
     const xScale = d3.scaleTime().domain([xMin, xMax]).range([0, width]);
-    const yScale = d3.scaleLinear().domain(yMin - 5, yMax).range([height, 0]);
+    const yScale = d3.scaleLinear().domain([yMin - 5, yMax]).range([height, 0]);
 
     svg.append('g')
         .attr('id', 'xAxis')
@@ -79,20 +75,21 @@ const drawLineChart = async (symbol: string, interval: string, options: Options)
         .attr('transform', `translate(${width}, 0)`)
         .call(d3.axisRight(yScale));
 
-    const line = d3
-        .line()
-        .x((datum: Datum) => xScale(datum.date))
-        .y((datum: Datum) => yScale(datum.close));
-
     svg.append('path')
         .data([data])
         .style('fill', 'none')
         .attr('id', 'priceChart')
         .attr('stroke', 'steelblue')
         .attr('stroke-width', '1.5')
-        .attr('d', line);
+        .attr('d', d3.line()
+            //@ts-ignore
+            .x(datum => xScale(datum.date))
+            //@ts-ignore
+            .y(datum => yScale(datum.close))
+        );
 }
 
 (async () => {
-    await drawLineChart('BTCUSDT', '1h', {});   
+    console.log('GAY IS RUNNING FROM COPS');
+    await drawLineChart('BTCUSDT', '1h', {});
 })();
